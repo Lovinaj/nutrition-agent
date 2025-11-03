4187bd824743437ee2a572eeb2a7f086fd224f16
lovinaj <lovejon218@gmail.com>
2025-11-03 12:24:57 +0100

    updated index.ts file

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -3,6 +3,9 @@ import { nutritionAgent } from './agents/nutrition-agent';
 
 // Create Mastra instance with your agent
 export const mastra = new Mastra({
+  bundler: {
+    externals: ['axios']
+  },
   agents: {
     nutritionAgent,
   },
